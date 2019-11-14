5da5f14717120f02af94b72654e1025b268be98b
Leandro Marques <leandrolmarques@gmail.com>
2019-11-14 09:32:08 -0300

    trying stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './App.css'
 
+// this gotta do it
+
 import movies from './data'
 import Header from './components/Header'
 import MovieList from './components/MovieList'
